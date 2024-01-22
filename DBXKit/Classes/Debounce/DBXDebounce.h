1285c28eefb9a1f399c878e4ab468a323fbb73cb
asherluo <asherluo@tencent.com>
2024-01-22 23:54:05 +0800

    增加防抖文件

---
+++ DBXKit/Classes/Debounce/DBXDebounce.h
@@ -0,0 +1,17 @@
+//
+//  DBXDebounce.h
+//  DBXKit
+//
+//  Created by 罗俊宇 on 2024/1/22.
+//  Copyright © 2024 DBX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBXDebounce : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DBXKit/Classes/Debounce/DBXDebounce.m
@@ -0,0 +1,13 @@
+//
+//  DBXDebounce.m
+//  DBXKit
+//
+//  Created by 罗俊宇 on 2024/1/22.
+//  Copyright © 2024 DBX. All rights reserved.
+//
+
+#import "DBXDebounce.h"
+
+@implementation DBXDebounce
+
+@end
