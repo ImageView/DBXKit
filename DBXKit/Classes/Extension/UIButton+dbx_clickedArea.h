d47866d8e3850c9d3336f4a9a65f6183190b3634
asherluo <asherluo@tencent.com>
2024-07-27 22:48:38 +0800

    新增支持修改按钮点击区域的功能

---
+++ DBXKit/Classes/Extension/UIButton+dbx_clickedArea.h
@@ -0,0 +1,27 @@
+//
+//  UIButton+dbx_clickedArea.h
+//  DBXKit
+//
+//  Created by 罗俊宇 on 2024/07/27.
+//  Copyright © 2024 DBX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton (dbx_clickedArea)
+
+/// 点击区域扩展范围
+@property (nonatomic, assign) UIEdgeInsets dbx_clickedAreaEdgeInsets;
+/// 设置点击区域，并且直接生肖
+/// 等同于clickedAreaEdgeInsets + mna_enableExtendedClickedArea
+- (void)dbx_setAndEnableClickedAreaEdgeInsets:(UIEdgeInsets)clickedAreaEdgeInsets;
+/// 扩展点击区域生效
+- (void)dbx_enableExtendedClickedArea;
+/// 扩展点击区域失效
+- (void)dbx_disableExtendedClickedArea;
+
+@end
+
+NS_ASSUME_NONNULL_END
